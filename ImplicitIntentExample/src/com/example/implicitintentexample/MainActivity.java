974fef582a9b428322bf392402d1800c7df84eaa
landonrobinson <toonboy1>
2014-02-18 17:16:26 -0500

    Baby's first push

---
+++ ImplicitIntentExample/src/com/example/implicitintentexample/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.implicitintentexample;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent("com.example.implicitintentexample.intent.action.VIEW");
+				intent.addCategory(Intent.CATEGORY_DEFAULT);
+				startActivity(intent);
+			}
+		});
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
